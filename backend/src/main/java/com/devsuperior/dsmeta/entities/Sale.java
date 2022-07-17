b2787d1216fc1f876b5fffeb63a3dc1b0d9a9cbf
ViniciusNeivas <viniciusneivas@outlook.com.br>
2022-07-17 15:43:31 -0300

    Sale Listing

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -26,53 +26,67 @@ public class Sale {
 		
 	}
 
+
 	public Long getId() {
 		return id;
 	}
+
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+
 	public String getSellerName() {
 		return sellerName;
 	}
+
 
 	public void setSellerName(String sellerName) {
 		this.sellerName = sellerName;
 	}
 
+
 	public Integer getVisited() {
 		return visited;
 	}
+
 
 	public void setVisited(Integer visited) {
 		this.visited = visited;
 	}
 
+
 	public Integer getDeals() {
 		return deals;
 	}
+
 
 	public void setDeals(Integer deals) {
 		this.deals = deals;
 	}
 
-	public Double getAmout() {
+
+	public Double getAmount() {
 		return amount;
 	}
 
-	public void setAmout(Double amout) {
-		this.amount = amout;
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
 	}
+
 
 	public LocalDate getDate() {
 		return date;
 	}
 
+
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
+
+	
 	
 	
 }
