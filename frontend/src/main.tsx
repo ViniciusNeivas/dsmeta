39625a2a55fa65e70552373a73009a369905da14
ViniciusNeivas <viniciusneivas@outlook.com.br>
2022-07-17 17:27:14 -0300

    HTMLElement In Main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 window.React = React
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
